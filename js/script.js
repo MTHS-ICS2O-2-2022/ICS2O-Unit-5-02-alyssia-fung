4b508d7aab9170922ac5d64142789aa20f6347fc
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-04-30 03:48:32 +0000

    fixed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Alyssia fung All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Alyssia fung
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit5-02-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-02-HTML/",
+  })
+}
+
+/**
+ * This function updates the slider value.
+ */
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
+}
+
+/**
+ * This function displays the slider value.
+ */
+function myButtonClicked() {
+  let buttonPositiveChecked = document.getElementById("positive").checked
+
+  if (buttonPositiveChecked == true) {
+    let randomNumber = Math.floor(Math.random() * 6)
+    document.getElementById("radio-button-value").innerHTML =
+      "Value is: " + randomNumber
+  } else {
+    let randomNumber = Math.floor(Math.random() * -6)
+    document.getElementById("radio-button-value").innerHTML =
+      "Value is: " + randomNumber
+  }
+}
+
