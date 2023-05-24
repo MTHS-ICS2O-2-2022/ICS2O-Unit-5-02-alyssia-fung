23279ab2be10600ccf1daee217945acad0417b9f
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-05-24 23:15:39 +0000

    fixed

--- js/script.js
+++ js/script.js
@@ -5,22 +5,6 @@
 // This file contains the JS functions for index.html
 
 /**
- * Check servie worker.
- */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit5-02-HTML/sw.js", {
-    scope: "/ICS2O-Unit5-02-HTML/",
-  })
-}
-
-/**
- * This function updates the slider value.
- */
-function updateSliderValue(valueFromSlider) {
-  document.getElementById("slider-value").innerHTML = valueFromSlider
-}
-
-/**
  * This function displays the slider value.
  */
 function myButtonClicked() {
